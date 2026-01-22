3f4e047bf3aeca7e62a67ca3c264e04307260ebe
helena-antoni <helenaantoni03@gmail.com>
2026-01-22 08:21:01 -0300

    refactor: update Header component styles and button dark mode classes

--- src/utils/styeButtons.ts
+++ src/utils/styeButtons.ts
@@ -1,6 +1,8 @@
 const BASE_BUTTON_CLASSES = 
 "flex items-center border-none justify-center gap-2 py-1.5 px-3 rounded-xl font-semibold text-sm transition duration-300";
 
+const BASE_BUTTON_CLASSES_HEADER = 
+"flex items-center border-none justify-center p-1.5 rounded-xl font-semibold text-sm transition duration-300";
 
 export const PRIMARY_BUTTON_CLASSES = `
   ${BASE_BUTTON_CLASSES}
@@ -13,4 +15,17 @@ export const SECONDARY_BUTTON_CLASSES = `
       text-[#4C2A00] hover:text-[#FFE8CC] hover:bg-[#663800]  cursor-pointer
 `;
 
- 
+
+
+
+export const PRIMARY_BUTTON_CLASSES_HEADER = `
+  ${BASE_BUTTON_CLASSES_HEADER}
+   text-[#FFE8CC] bg-[#4C2A00] hover:text-[#FFE8CC] hover:bg-[#663800] shadow-md cursor-pointer
+  
+`;
+
+export const SECONDARY_BUTTON_CLASSES_HEADER = `
+  ${BASE_BUTTON_CLASSES_HEADER}
+      text-[#4C2A00] hover:text-[#FFE8CC] hover:bg-[#663800]  cursor-pointer
+`;
+

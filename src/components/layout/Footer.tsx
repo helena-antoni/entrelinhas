743b305bbd47fadbcfee8ef07e2a79eead467e1c
helena-antoni <helenaantoni03>
2026-02-05 21:22:28 -0300

    feat: update footer component and replace line SVGs
    - Updated Footer component to use responsive line SVGs for different screen sizes.
    - Removed the old line-between-lines.svg file.
    - Added new SVG files: between-lines-dektop.svg, between-lines-tablet.svg, and between-lines-mobile.svg.
    - Adjusted layout and styling in the Footer for improved responsiveness and visual consistency.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import Image from 'next/image';
-import heartfooter from 'heart-footer.svg';
-import line from '../../assets/line-between-lines.svg';
+import heartfooter from 'heart-footer.svg'; 
+
+import lineDesktop from '../../assets/between-lines-dektop.svg';
+import lineTablet from '../../assets/between-lines-tablet.svg';
+import lineMobile from '../../assets/between-lines-mobile.svg';
   
 
 const Footer: React.FC = () => {  
    return (
     <footer className="w-full bg-[#FFE8CC] mt-[133px] md:mt-52 lg:mt-40
         relative z-20 overflow-hidden
-        shadow-[0_-12px_16px_-4px_rgba(0,0,0,0.08),0_4px_6px_-2px_rgba(0,0,0,0.03)]">
-        <div className=" 
-            max-w-[1440px] mx-auto 
-            px-6 text-center  sm:px-12 lg:px-24 
-            flex flex-col  gap-8 md:gap-12 lg:gap-0                
-            lg:flex-row lg:justify-between lg:items-center  ">
-                <div className="
-                    w-full 
-                    lg:w-auto 
-                ">
-                    <a href="/" className="flex justify-center lg:justify-start">
-                        <Image  
-                            src={'/logo.svg'}
-                            alt="Logo Entrelinhas" 
-                            width={120} 
-                            height={30}
-                            className="h-8 w-auto opacity-70" 
-                        />
-                    </a>
-                </div>
+        shadow-[0_-12px_16px_-4px_rgba(0,0,0,0.08),0_4px_6px_-2px_rgba(0,0,0,0.03)]
+        pt-8 pb-4 md:pt-12 md:pb-6 
+            px-6 md:px-[48px] lg:px-[96px]">
 
-                {/* info e crédito */}
-                <div className=" w-full lg:w-auto md:border-l md:border-gray-400/50 md:pl-8 lg:pl-10 lg:text-right text-[#4C2A00]  text-[16px] ">
+        <div className="  mx-auto lg:m-auto  max-w-[1248px]
+            flex flex-col md:flex-row items-start md:items-start gap-8  lg:gap-0">
+                
+            <div className=" flex justify-start lg:px-12 lg:py-6 ">
+                <a href="/" className=" ">
+                    <Image  
+                        src={'/logo.svg'}
+                        alt="Logo Entrelinhas" 
+                        width={102} 
+                        height={24} 
+                    />
+                </a>
+            </div>
+
+            {/* info e crédito */}
+            <div className="w-full md:w-auto lg:m-auto md:m-auto md:border-l lg:border-l  border-[#4C2A00] 
+                flex flex-col items-start  text-start   md:text-start
+                md:pl-10 text-[#4C2A00]">
                     
-                    <p className=" font-indie font-normal text-xl md:text-2xl  leading-[-22%] mb-2 lg:mb-1 ">
-                        "Entrelinhas – porque o que importa está no meio das linhas."
-                    </p>
+                <p className=" font-indie font-normal text-[22px] pb-6
+                    md:text-2xl  leading-[-22%]  tracking-[140%]   ">
+                    "Entrelinhas – porque o que importa está no meio das linhas."
+                </p>
 
-                    <p className={`leading-[150%] font-medium text-left`}>
-                        &copy; 2026 Entrelinhas. Todos os direitos reservados.
-                    </p>
+                <p className={`leading-[150%] font-medium text-left pb-2 `}>
+                    &copy; 2026 Entrelinhas. All rights reserved.
+                </p>
 
-                    <p className={` flex items-center justify-center lg:justify-start gap-1  
-                        mt-3 text-xs sm:text-sm leading-[150%] text-left  font-inter font-normal
-                        lg:text-left `}> 
+                <p className={` flex flex-wrap items-center justify-start lg:justify-end   text-sm 
+                    font-medium text-[16px]  leading-[200%]  gap-1
+                    `}> 
                         Feito com 
                         <Image 
                             src={"/heart-footer.svg"}
@@ -55,28 +58,34 @@ const Footer: React.FC = () => {
                         <a 
                         target="_blank"
                         href='https://github.com/estercopinheiro'
-                        className={"font-bold"}> <span className=' bg-[#FFAE4C] p-1.5 rounded-[12px] hover:bg-[#FFF3E5]'>Ester Pinheiro</span> </a> 
-                        e
+                        className={" "}> <span className=' bg-[#FFAE4C] p-1.5 rounded-[12px] hover:bg-[#FFF3E5]'>Ester Pinheiro</span> </a> 
+                        &
                         <a 
                         target="_blank"
                         href='https://github.com/helena-antoni'
-                        className={"font-bold"}> <span className='bg-[#FFAE4C] p-1.5 rounded-[12px] hover:bg-[#FFF3E5]'>Helena Antoni</span> </a>
+                        className={" "}> <span className='bg-[#FFAE4C] p-1.5 rounded-[12px] hover:bg-[#FFF3E5]'>Helena Antoni</span> </a>
                     </p>
                 </div>
 
             </div>
             
-            <div className="mx-auto w-full max-w-[1440px]  md:pt-2.5 lg:pt-2.5 pt-2 flex-shrink-0">
-                <Image  
-                    src={line} 
-                    alt="Linha laranja" 
-                    width={1248}  
-                    height={47}
-                    className="w-full h-auto" 
+
+        <div className="lg:w-[1248px] m-auto mt-auto  ">
+            <picture className="   pt-2 pb-8 px-6 ">
+                <source media="(min-width: 1024px)" srcSet={lineDesktop.src} />
+                
+                <source media="(min-width: 768px)" srcSet={lineTablet.src} />
+                
+                <img 
+                    src={lineMobile.src} 
+                    alt="Linha decorativa" 
+                    className="w-full h-auto object-contain"
+                    style={{ maxHeight: '100px' }}
                 />
-            </div>
+            </picture>
+        </div>
         </footer>
     );
-};
+}
 
 export default Footer;
